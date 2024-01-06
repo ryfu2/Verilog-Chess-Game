4c7f5568d81d4e7910a859a303171ad20f73eebe
Ryan Yuyang Fu <102570521+ryfu2@users.noreply.github.com>
2024-01-05 23:51:03 -0600

    Update chessboard_example.sv

--- COE/chessboard/chessboard_example.sv
+++ COE/chessboard/chessboard_example.sv
@@ -1,11 +1,11 @@
 module chessboard_example (
 	input logic vga_clk,
 	input logic [9:0] DrawX, DrawY,
-	input logic blank,
-	output logic [3:0] red, green, blue
+	output logic [3:0] red, green, blue,
+	output logic boardon
 );
 
-logic [15:0] rom_address;
+logic [17:0] rom_address;
 logic [3:0] rom_q;
 
 logic [3:0] palette_red, palette_green, palette_blue;
@@ -17,17 +17,22 @@ assign negedge_vga_clk = ~vga_clk;
 
 // address into the rom = (x*xDim)/640 + ((y*yDim)/480) * xDim
 // this will stretch out the sprite across the entire screen
-assign rom_address = ((DrawX * 240) / 640) + (((DrawY * 240) / 480) * 240);
 
 always_ff @ (posedge vga_clk) begin
-	red <= 4'h0;
-	green <= 4'h0;
-	blue <= 4'h0;
-
-	if (blank) begin
 		red <= palette_red;
 		green <= palette_green;
 		blue <= palette_blue;
+end
+
+always_comb begin
+	if (DrawX >= 80 && DrawX <= 559) begin
+		boardon  = 1;
+//		rom_address = (DrawX - 80)/2 + DrawY * 120;
+        rom_address = ((DrawX - 80) * 240) / 480 + (((DrawY * 240) / 480) * 240);
+    end
+	else begin
+		boardon = 0;
+		rom_address = 0;
 	end
 end
 
